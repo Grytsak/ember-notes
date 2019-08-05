7bebd784fb9f9a52e55172acb67c444cbcc15a34
Oleh Hrytsak <grytsak.oleg@gmail.com>
2019-08-05 19:41:08 +0300

    feat(starting components): add 3 start components

---
+++ app/components/notes-sidebar.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import Ember from 'ember';
+
+export default Component.extend({
+	classNames: ['notes-sidebar'],
+	tagName: 'aside'
+});
+
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,23 +2,32 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
-    // Add options here
-  });
+module.exports = function (defaults) {
+	let app = new EmberApp(defaults, {
+		nodeAssets: {
+			'simple-css-reset': {
+				import: ['reset.css']
+			},
+            'ember-froala-editor': {
+                languages: ['es'],
+                plugins  : true,
+                themes   : 'royal'
+            },
+		}
+	});
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+	// Use `app.import` to add additional libraries to the generated
+	// output files.
+	//
+	// If you need to use different assets in different
+	// environments, specify an object as the first parameter. That
+	// object's keys should be the environment name and the values
+	// should be the asset to use in that environment.
+	//
+	// If the library that you are including contains AMD or ES6
+	// modules that you would like to import into your application
+	// please specify an object with the list of modules as keys
+	// along with the exports of each module as its value.
 
-  return app.toTree();
+	return app.toTree();
 };
